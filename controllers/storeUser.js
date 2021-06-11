1206545ed262e301e26a07747edaa963f1dbe5de
s-bot12 <lkns0989@gmail.com>
2021-06-12 03:49:11 +0900

    change DB

--- controllers/storeUser.js
+++ controllers/storeUser.js
@@ -5,11 +5,11 @@ const Rank = require('../models/Rank.js')
 module.exports = (req,res)=>{
     User.create(req.body, (error, user) => {
         if(error) {
-            alert('이미 존재하는 아이디 입니다.')
+
             return res.redirect('/login')
         }
         else {
-            alert('회원가입 성공!')
+
             Rank.create({
                 ...req.body,
                 name: user.name,
